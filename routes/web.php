9b7b1cab06d3f9dfef9d842a6e04b1dbf28ee361
HojiakbarSultonov <hojiakbarrasulovich@gmail.com>
2023-06-14 11:00:27 +0500

    Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-route', function (){
+    return view('test');
+});
+
+//Route::get('/users', function (){
+//    return 'User';
+//});
+
+Route::get('/users/{id?}', function ($user_id=null){
+    return 'siz tanlagan User id bu : ' . $user_id ;
+});
+
+Route::view('tests', 'test');
+
+Route::get('/users/{id?}', function ($user_id=null){
+    return 'siz tanlagan User id bu : ' . $user_id ;
+})->name('users');
+
+
+Route::prefix('admin')->group(function (){
+    Route::get('/users', function (){
+        return 'admin user'
+    })
+})
+}
+
+
+
+//Route::post('contact', function (){
+//    return 'post route';
+//});
+
+
+Route::post('contact', [Controller::class, 'action']);
+
